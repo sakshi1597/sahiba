b918e1f1f53f691210413a7e82023a7dbdc9f650
Sakshi Gupta <guptasakshi.1529@gmail.com>
2020-06-28 17:06:53 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+const port = process.env.PORT || 3001
 //Define Path for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname,'../templates//views')
@@ -110,6 +111,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3001, () => {
-    console.log('Server is up on port 3001.')
+app.listen(port, () => {
+    console.log('Server is up on port.' +port)
 })
